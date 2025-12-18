7bb4be65c0aace49df2b640527e1059a9316179a
kentakom1213 <kentakom1213@gmail.com>
2025-12-18 16:15:45 +0900

    fix: 修正

--- .config/kyopro/util.sh
+++ .config/kyopro/util.sh
@@ -6,11 +6,7 @@
 export KYOPRO_DIR=$HOME/develop/algo/kyopro
 export KYOPURO_LIBRARY_DIR=$HOME/develop/algo/cp-library/cp-library-rs
 
-# バンドラ
-export BUNDLER='/Users/komotokenta/develop/algo/cp-library/expander/target/release/expand'
-
 # エイリアス
 alias compete=$HOME/.config/kyopro/compete.sh
 
-alias bundle="$BUNDLER $KYOPURO_LIBRARY_DIR"
-alias bundler=$BUNDLER
+alias bundle="bundler $KYOPURO_LIBRARY_DIR"
