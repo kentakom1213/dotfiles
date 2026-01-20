a12c0301ad13f63a190078d68b85d9873c37d623
kentakom1213 <kentakom1213@gmail.com>
2026-01-20 15:44:08 +0900

    fix: bundler の修正

--- .config/kyopro/util.sh
+++ .config/kyopro/util.sh
@@ -4,9 +4,8 @@
 
 # パス
 export KYOPRO_DIR=$HOME/develop/algo/kyopro
-export KYOPURO_LIBRARY_DIR=$HOME/develop/algo/cp-library/cp-library-rs
 
 # エイリアス
 alias compete=$HOME/.config/kyopro/compete.sh
 
-alias bundle="~/.local/bin/bundler $KYOPURO_LIBRARY_DIR"
+alias bundle="~/.local/bin/bundler --crate-name cp-library-rs"
