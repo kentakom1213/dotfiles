7b9c4591b19782ecd22bd9d4403a5c17d868a534
kentakom1213 <kentakom1213@gmail.com>
2026-02-26 14:13:53 +0900

    fix: 環境の反映

--- .config/kyopro/util.sh
+++ .config/kyopro/util.sh
@@ -3,9 +3,11 @@
 # 競プロ支援ツール
 
 # パス
-export KYOPRO_DIR=$HOME/develop/algo/kyopro
+export KYOPRO_DIR="$HOME/develop/algo/kyopro"
+export KYOPRO_LIBRARY_DIR="$HOME/develop/algo/cp-library/cp-library-rs"
 
 # エイリアス
 alias compete=$HOME/.config/kyopro/compete.sh
 
+alias bundle_local="~/.local/bin/bundler -l $KYOPRO_LIBRARY_DIR"
 alias bundle="~/.local/bin/bundler --crate-name cp-library-rs"
