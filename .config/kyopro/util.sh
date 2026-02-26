67501d3f0e56bf1c71fb64ae5d79c5f51920c76c
kentakom1213 <kentakom1213@gmail.com>
2026-02-26 11:38:03 +0900

    add: 設定の修正

--- .config/kyopro/util.sh
+++ .config/kyopro/util.sh
@@ -5,9 +5,10 @@
 # パス
 export KYOPRO_DIR=$HOME/develop/algo/kyopro
 local KYOPRO_UTIL_DIR=$HOME/develop/algo/cp-library
-export KYOPURO_LIBRARY_DIR=$KYOPRO_UTIL_DIR/cp-library-rs
+export KYOPRO_LIBRARY_DIR=$KYOPRO_UTIL_DIR/cp-library-rs
 
 # エイリアス
 alias compete=$HOME/.config/kyopro/compete.sh
 
-alias bundle='bundler $KYOPURO_LIBRARY_DIR'
+alias bundle='bundler --crate-name cp-library-rs'
+alias bundle_local="bundler --library $KYOPRO_LIBRARY_DIR"
